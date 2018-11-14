3784b4e5be890d2928f16f639a391be6649686b7
jkerns21 <43002620+jkerns21@users.noreply.github.com>
2018-11-14 08:37:47 -0500

    Add files via upload

---
+++ src/WhichSeason.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class WhichSeason 
+{
+	public static void main (String []args)
+	{
+		Scanner keyboard = new Scanner(Sysem.in);
+		System.out.println("Please enter a month number from 1  to 12...");
+		int x = keyboard.nextInt();
+		if ((x>12)||(x<1))
+		{
+			System.out.println("This is an invalid month. ");
+		}
+		else if (x==)
+	}
+}
