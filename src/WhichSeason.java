427fe4e24ed766f8a44b41a95760334ef94dbc80
jkerns21 <43002620+jkerns21@users.noreply.github.com>
2018-11-15 13:04:48 -0500

    Add files via upload
    A

--- src/WhichSeason.java
+++ src/WhichSeason.java
@@ -2,15 +2,32 @@ import java.util.Scanner;
 
 public class WhichSeason 
 {
+
 	public static void main (String []args)
 	{
-		Scanner keyboard = new Scanner(Sysem.in);
-		System.out.println("Please enter a month number from 1  to 12...");
+		Scanner keyboard = new Scanner(System.in);
+		System.out.println("Please enter your birth month...");
 		int x = keyboard.nextInt();
-		if ((x>12)||(x<1))
+		if ((x<1)||(x>12))	
 		{
 			System.out.println("This is an invalid month. ");
 		}
-		else if (x==)
+		else if ((x<3)||(x==12))
+		{
+			System.out.println("You were born in the Winter.");
+		}
+		else if ((x>2)&&(x<6))
+		{
+			System.out.println("You were born in the Spring.");
+		}
+		else if ((x>5)&&(x<9))
+		{
+			System.out.println("You were born in the Summer.");
+		}
+		else
+		{
+			System.out.println("You were born in the Fall.");
+		}
+		
 	}
 }
